dedd96f1fc27c6ba58314d935356d30aaf4cb05b
Domt301 <leonardod.trimarchi@gmail.com>
2016-12-07 08:53:42 -0500

    changes

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var fortune = require('./lib/fortune.js');
+var express = require('express'),
+	fortune = require('./lib/fortune.js'),
+	  formidable = require('formidable');
 
 var app = express();
+
+var credentials = require('./lib/credentials.js');
 
 //set up handlebars view engine
 var handlebars = require('express-handlebars').create({
@@ -22,7 +25,24 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
+
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
+
+// flash message middleware
+app.use(function(req, res, next) {
+    // if there's a flash message, transfer
+    // it to the context, then clear it
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
 
 
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
@@ -30,7 +50,6 @@ app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redi
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 
-app.use(require('body-parser').urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
@@ -114,21 +133,71 @@ app.get('/data/nursery-rhyme', function(req, res) {
     });
 });
 
-app.get('/newsletter', function(req, res) {
-    // we will learn about CSRF later...for now, we just
-    // provide a dummy value
-    res.render('newsletter', { csrf: 'CSRF token goes here' });
-});
-app.post('/process', function(req, res) {
-    if (req.xhr || req.accepts('json,html') === 'json') {
-        // if there were an error, we would send { error: 'error description' }
-        res.send({ success: true });
-    } else {
-        // if there were an error, we would redirect to an error page
-        res.redirect(303, '/thank-you');
-    }
+app.get('/thank-you', function(req, res) {
+    res.render('thank-you');
 });
 
+app.get('/newsletter', function(req, res) {
+    res.render('newsletter');
+});
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup() {}
+NewsletterSignup.prototype.save = function(cb) {
+    cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res) {
+    var name = req.body.name || '',
+        email = req.body.email || '';
+    // input validation
+    if (!email.match(VALID_EMAIL_REGEX)) {
+        if (req.xhr) return res.json({ error: 'Invalid name email address.' });
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was  not valid.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+    new NewsletterSignup({ name: name, email: email }).save(function(err) {
+        if (err) {
+            if (req.xhr) return res.json({ error: 'Database error.' });
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.',
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if (req.xhr) return res.json({ success: true });
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
+});
+app.get('/newsletter/archive', function(req, res) {
+    res.render('newsletter/archive');
+});
+app.get('/contest/vacation-photo', function(req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        if (err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
 
 //404 catch-all handler (middleware)
 app.use(function(req, res, next) {
